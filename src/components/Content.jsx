3899981f3688663b45cbecf29cb4f2da835c454e
hanancs <benhanan.subendran@gmail.com>
2021-09-18 17:43:33 +0530

    Responsive Changes added

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,7 +5,6 @@ import { AppContext } from './../App/AppContext';
 
 const sharedStyles = css`
   transition: color 0.5s linear;
-  font-weight: 900;
   position: relative;
   z-index: 1;
 `;
@@ -13,20 +12,22 @@ const sharedStyles = css`
 const C = {
   Name: styled.h1`
     ${sharedStyles};
-    font-size: 6rem;
+    font-size: 10rem;
+    font-weight: 900;
     margin: 0 0 3.5rem;
     color: ${({ theme }) => theme.primaryTextColor};
     @media only screen and (max-device-width: 820px) and (-webkit-min-device-pixel-ratio: 2) {
-      font-size: 4.5rem;
+      font-size: 3.5rem;
     }
   `,
   Title: styled.h2`
     ${sharedStyles};
     font-size: 3.5rem;
     margin: 4rem 0;
+    font-weight: 0;
     color: ${({ theme }) => theme.secondaryTextColor};
     @media only screen and (max-device-width: 820px) and (-webkit-min-device-pixel-ratio: 2) {
-      font-size: 2.5rem;
+      font-size: 2rem;
     }
   `,
 };
@@ -36,7 +37,7 @@ export const Content = () => {
 
   return (
     <div>
-      <C.Name theme={theme} data-v2='name' aria-label='HananStems Records'>HananStems Records</C.Name>
+      <C.Name theme={theme} data-v2='name' aria-label='HananStems Records'>HANANSTEMS RECORDS</C.Name>
       <C.Title theme={theme} data-v2='title' aria-label='Record Label | Music Production Studio'>RECORD LABEL | MUSIC PRODUCTION STUDIO</C.Title>
     </div>
   );
